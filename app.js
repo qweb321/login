b7ec399d1e65cdd5d6ed8182d4d8db25984cbfce
Isis Lin <qweb32114@gmail.com>
2022-11-21 17:04:57 +0800

    feat: add cookieParse to save login status and add profile page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const users = require("./modules/users");
 
 const port = 3000;
@@ -9,6 +10,7 @@ const app = express();
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -18,12 +20,26 @@ app.post("/", (req, res) => {
   for (const user of users) {
     if (user.email === req.body.useremail) {
       if (user.password === req.body.password) {
+        res.cookie("user", user.firstName);
         return res.render("welcome", { Name: user.firstName });
       } else {
         return res.render("home", { error: "Email or Password not correct" });
       }
     }
   }
+});
+
+app.get("/detail", (req, res) => {
+  let Edituser = "";
+  if (!Object.keys(req.cookies).length) {
+    return res.redirect("/");
+  }
+  for (const user of users) {
+    if (user.firstName === req.cookies.user) {
+      Edituser = user;
+    }
+  }
+  res.render("detail", { User: Edituser });
 });
 
 app.listen(port, () =>
